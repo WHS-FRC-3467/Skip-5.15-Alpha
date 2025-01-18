7b3f5111adae0cda09bad851b48a221b8ec56ed9
Noah-H3467 <nhaskell26@team3467.org>
2025-01-18 10:58:28 -0500

    Work in progress - copy templates for simple and complex versions of climber sub

---
+++ src/main/java/frc/robot/subsystems/Climber/SampleRollersIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.Climber;
+
+import frc.robot.subsystems.GenericRollerSubsystem.GenericRollerSubsystemIO;
+
+public interface SampleRollersIO extends GenericRollerSubsystemIO {}
---
+++ src/main/java/frc/robot/subsystems/ComplexClimber/SampleProfiledArmIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.ComplexClimber;
+
+import frc.robot.subsystems.GenericMotionProfiledSubsystem.GenericMotionProfiledSubsystemIO;
+
+public interface SampleProfiledArmIO extends GenericMotionProfiledSubsystemIO {}
