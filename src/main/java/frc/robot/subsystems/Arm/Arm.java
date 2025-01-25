000ef6c5ff600b489fb9720d21e4d614fdf144d1
Driver3467 <frc_3467_driver@team3467.org>
2025-01-25 12:25:26 -0500

    Finalized

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -17,26 +17,26 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
   @RequiredArgsConstructor
   @Getter
   public enum State implements TargetState {
-    HOME(0.0, 0.0),
-    HOMING(0.0, 0.0),
-    LEVEL_1(Units.degreesToRotations(90.0), 0.0),
-    LEVEL_2(Units.degreesToRotations(135.0), 0.0),
-    LEVEL_3(Units.degreesToRotations(135.0), 0.0),
-    LEVEL_4(Units.degreesToRotations(200.0), 0.0);
+    HOME(0.0, 0.0, ProfileType.MM_POSITION),
+    HOMING(0.0, 0.0, ProfileType.MM_POSITION),
+    LEVEL_1(Units.degreesToRotations(90.0), 0.0, ProfileType.MM_POSITION),
+    LEVEL_2(Units.degreesToRotations(135.0), 0.0, ProfileType.MM_POSITION),
+    LEVEL_3(Units.degreesToRotations(135.0), 0.0, ProfileType.MM_POSITION),
+    LEVEL_4(Units.degreesToRotations(200.0), 0.0, ProfileType.MM_POSITION);
 
     private final double output;
     private final double feedFwd;
 
-    @Override
-    public ProfileType getProfileType() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'getProfileType'");
-    }
+    private final ProfileType profileType;
   }
 
   @Getter @Setter private State state = State.HOME;
 
   private final boolean debug = true;
+
+  public Arm(ArmIO io, boolean isSim) {
+    super(ProfileType.MM_POSITION, ArmConstants.kSubSysConstants, io, isSim);
+  }
 
   public Trigger homedTrigger =
       new Trigger(
@@ -44,10 +44,6 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
               (this.state == State.HOMING && io.getSupplyCurrent() > ArmConstants.kHomingCurrent));
 
   /** Constructor */
-  public Arm(ArmIO io, boolean isSim) {
-    super(ProfileType.MM_POSITION, ArmConstants.kSubSysConstants, io, isSim);
-  }
-
   public Command setStateCommand(State state) {
     return startEnd(() -> this.state = state, () -> this.state = State.HOME);
   }
