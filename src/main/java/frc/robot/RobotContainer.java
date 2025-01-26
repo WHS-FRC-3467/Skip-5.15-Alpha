cce9e9d1d8ff728298f8e57743fd4b1ae07b7310
skukshtel <scottkukshtel@team3467.org>
2025-01-26 17:10:21 -0500

    Modify file formatter and provide format config file for customizing format conventions. Reformat GenericMotionProfiledSubsystem. Modify PID gains and position target for SampleProfiledRoller.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -52,228 +52,226 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
 
-  // Controllers
-  private final CommandXboxController m_driver = new CommandXboxController(0);
-  // private final CommandXboxController m_operator = new
-  // CommandXboxController(1);
+    // Controllers
+    private final CommandXboxController m_driver = new CommandXboxController(0);
+    private final CommandXboxController m_operator = new CommandXboxController(1);
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> m_autoChooser;
+    // Dashboard inputs
+    private final LoggedDashboardChooser<Command> m_autoChooser;
 
-  // AK-enabled Subsystems
-  private final Drive m_drive;
-  private final SampleRollers m_sampleRollersSubsystem;
-  private final Arm m_sampleArmSubsystem;
-  private final SampleProfiledElevator m_sampleElevatorSubsystem;
-  private final SampleProfiledRoller m_sampleProfiledRollerSubsystem;
+    // AK-enabled Subsystems
+    private final Drive m_drive;
+    private final SampleRollers m_sampleRollersSubsystem;
+    private final Arm m_sampleArmSubsystem;
+    private final SampleProfiledElevator m_sampleElevatorSubsystem;
+    private final SampleProfiledRoller m_sampleProfiledRollerSubsystem;
 
-  public final Vision m_vision;
+    public final Vision m_vision;
 
-  // Non-AK-enabled Subsystems
-  // private final SimpleSubsystem m_simpleSubsystem = new SimpleSubsystem();
-  // private final ComplexSubsystem m_complexSubsystem = new ComplexSubsystem();
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer()
+    {
+        switch (Constants.currentMode) {
+            case REAL:
+                // Real robot, instantiate hardware IO implementations
+                m_drive =
+                    new Drive(
+                        new GyroIOPigeon2(),
+                        new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                        new ModuleIOTalonFX(TunerConstants.FrontRight),
+                        new ModuleIOTalonFX(TunerConstants.BackLeft),
+                        new ModuleIOTalonFX(TunerConstants.BackRight));
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+                m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIOTalonFX());
+                m_sampleArmSubsystem = new Arm(new ArmIOTalonFX(), false);
+                m_sampleElevatorSubsystem =
+                    new SampleProfiledElevator(new SampleProfiledElevatorIOTalonFX(), false);
+                m_sampleProfiledRollerSubsystem =
+                    new SampleProfiledRoller(new SampleProfiledRollerIOTalonFX(), false);
 
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        m_drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
+                m_vision =
+                    new Vision(
+                        m_drive::addVisionMeasurement,
+                        new VisionIOPhotonVision(camera0Name, robotToCamera0),
+                        new VisionIOPhotonVision(camera1Name, robotToCamera1));
 
-        m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIOTalonFX());
-        m_sampleArmSubsystem = new Arm(new ArmIOTalonFX(), false);
-        m_sampleElevatorSubsystem =
-            new SampleProfiledElevator(new SampleProfiledElevatorIOTalonFX(), false);
-        m_sampleProfiledRollerSubsystem =
-            new SampleProfiledRoller(new SampleProfiledRollerIOTalonFX(), false);
+                break;
 
-        m_vision =
-            new Vision(
-                m_drive::addVisionMeasurement,
-                new VisionIOPhotonVision(camera0Name, robotToCamera0),
-                new VisionIOPhotonVision(camera1Name, robotToCamera1));
+            case SIM:
+                // Sim robot, instantiate physics sim IO implementations
+                m_drive =
+                    new Drive(
+                        new GyroIO() {},
+                        new ModuleIOSim(TunerConstants.FrontLeft),
+                        new ModuleIOSim(TunerConstants.FrontRight),
+                        new ModuleIOSim(TunerConstants.BackLeft),
+                        new ModuleIOSim(TunerConstants.BackRight));
 
-        break;
+                m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIOSim());
+                m_sampleArmSubsystem = new Arm(new ArmIOSim(), true);
+                m_sampleElevatorSubsystem =
+                    new SampleProfiledElevator(new SampleProfiledElevatorIOSim(), true);
+                m_sampleProfiledRollerSubsystem =
+                    new SampleProfiledRoller(new SampleProfiledRollerIOSim(), true);
 
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        m_drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
+                m_vision =
+                    new Vision(
+                        m_drive::addVisionMeasurement,
+                        new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, m_drive::getPose),
+                        new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, m_drive::getPose));
+                break;
 
-        m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIOSim());
-        m_sampleArmSubsystem = new Arm(new ArmIOSim(), true);
-        m_sampleElevatorSubsystem =
-            new SampleProfiledElevator(new SampleProfiledElevatorIOSim(), true);
-        m_sampleProfiledRollerSubsystem =
-            new SampleProfiledRoller(new SampleProfiledRollerIOSim(), true);
+            default:
+                // Replayed robot, disable IO implementations
+                m_drive =
+                    new Drive(
+                        new GyroIO() {},
+                        new ModuleIO() {},
+                        new ModuleIO() {},
+                        new ModuleIO() {},
+                        new ModuleIO() {});
+                m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIO() {});
+                m_sampleArmSubsystem = new Arm(new ArmIO() {}, true);
+                m_sampleElevatorSubsystem =
+                    new SampleProfiledElevator(new SampleProfiledElevatorIO() {}, true);
+                m_sampleProfiledRollerSubsystem =
+                    new SampleProfiledRoller(new SampleProfiledRollerIO() {}, false);
 
-        m_vision =
-            new Vision(
-                m_drive::addVisionMeasurement,
-                new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, m_drive::getPose),
-                new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, m_drive::getPose));
-        break;
+                m_vision =
+                    new Vision(m_drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
 
-      default:
-        // Replayed robot, disable IO implementations
-        m_drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        m_sampleRollersSubsystem = new SampleRollers(new SampleRollersIO() {});
-        m_sampleArmSubsystem = new Arm(new ArmIO() {}, true);
-        m_sampleElevatorSubsystem =
-            new SampleProfiledElevator(new SampleProfiledElevatorIO() {}, true);
-        m_sampleProfiledRollerSubsystem =
-            new SampleProfiledRoller(new SampleProfiledRollerIO() {}, false);
+                break;
+        }
 
-        m_vision = new Vision(m_drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
+        // Set up auto routines
+        m_autoChooser =
+            new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
-        break;
+        // Set up SysId routines
+        m_autoChooser.addOption(
+            "Drive Wheel Radius Characterization",
+            DriveCommands.wheelRadiusCharacterization(m_drive));
+        m_autoChooser.addOption(
+            "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(m_drive));
+        m_autoChooser.addOption(
+            "Drive SysId (Quasistatic Forward)",
+            m_drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        m_autoChooser.addOption(
+            "Drive SysId (Quasistatic Reverse)",
+            m_drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        m_autoChooser.addOption(
+            "Drive SysId (Dynamic Forward)", m_drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        m_autoChooser.addOption(
+            "Drive SysId (Dynamic Reverse)", m_drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+        // Configure the controller button and joystick bindings
+        configureControllerBindings();
+
+        // Detect if controllers are missing / Stop multiple warnings
+        DriverStation.silenceJoystickConnectionWarning(true);
     }
 
-    // Set up auto routines
-    m_autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    // Set up SysId routines
-    m_autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(m_drive));
-    m_autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(m_drive));
-    m_autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        m_drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    m_autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        m_drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    m_autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", m_drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    m_autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", m_drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
-    // Configure the controller button and joystick bindings
-    configureControllerBindings();
-
-    // Detect if controllers are missing / Stop multiple warnings
-    DriverStation.silenceJoystickConnectionWarning(true);
-  }
-
-  /** Use this method to define your joystick and button -> command mappings. */
-  private void configureControllerBindings() {
-
-    // Default command, normal field-relative drive
-    m_drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            m_drive,
-            () -> -m_driver.getLeftY(),
-            () -> -m_driver.getLeftX(),
-            () -> -m_driver.getRightX()));
-
-    // Driver A Button: Lock to 0°
-    m_driver
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
+    /** Use this method to define your joystick and button -> command mappings. */
+    private void configureControllerBindings()
+    {
+        // Default command, normal field-relative drive
+        m_drive.setDefaultCommand(
+            DriveCommands.joystickDrive(
                 m_drive,
                 () -> -m_driver.getLeftY(),
                 () -> -m_driver.getLeftX(),
-                () -> new Rotation2d()));
+                () -> -m_driver.getRightX()));
 
-    // Driver X Button: Switch wheel modules to X pattern
-    m_driver.x().onTrue(Commands.runOnce(m_drive::stopWithX, m_drive));
+        // Driver A Button: Lock to 0°
+        m_driver
+            .a()
+            .whileTrue(
+                DriveCommands.joystickDriveAtAngle(
+                    m_drive,
+                    () -> -m_driver.getLeftY(),
+                    () -> -m_driver.getLeftX(),
+                    () -> new Rotation2d()));
 
-    // Driver B Button: Reset gyro to 0°
-    m_driver
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        m_drive.setPose(
-                            new Pose2d(m_drive.getPose().getTranslation(), new Rotation2d())),
+        // Driver X Button: Switch wheel modules to X pattern
+        m_driver.x().onTrue(Commands.runOnce(m_drive::stopWithX, m_drive));
+
+        // Driver B Button: Reset gyro to 0°
+        m_driver
+            .b()
+            .onTrue(
+                Commands.runOnce(
+                    () -> m_drive.setPose(
+                        new Pose2d(m_drive.getPose().getTranslation(), new Rotation2d())),
                     m_drive)
-                .ignoringDisable(true));
+                    .ignoringDisable(true));
 
-    // // Driver X Button: Run the Sample Roller in Eject direction when held
-    // m_driver.x().whileTrue(m_sampleRollersSubsystem.setStateCommand(SampleRollers.State.EJECT));
-    // // Driver Y Button: Run the Sample Roller in Intake direction when held
-    // m_driver.y().whileTrue(m_sampleRollersSubsystem.setStateCommand(SampleRollers.State.INTAKE));
+        // Driver Left & Right Bumpers: Run the Sample Profiled Roller out and in when held
+        m_driver
+            .leftBumper()
+            .whileTrue(
+                m_sampleProfiledRollerSubsystem.setStateCommand(SampleProfiledRoller.State.EJECT));
+        m_driver
+            .rightBumper()
+            .whileTrue(
+                m_sampleProfiledRollerSubsystem.setStateCommand(SampleProfiledRoller.State.INTAKE));
 
-    // Driver Left & Right Bumpers: Run the Sample Profiled Roller out and in when
-    // held
-    m_driver
-        .leftBumper()
-        .whileTrue(
-            m_sampleProfiledRollerSubsystem.setStateCommand(SampleProfiledRoller.State.EJECT));
-    m_driver
-        .rightBumper()
-        .whileTrue(
-            m_sampleProfiledRollerSubsystem.setStateCommand(SampleProfiledRoller.State.INTAKE));
+        // Driver Right Trigger: Run the Sample Profiled Roller to the requested position
+        m_driver
+            .rightTrigger()
+            .whileTrue(
+                m_sampleProfiledRollerSubsystem
+                    .setStateCommand(SampleProfiledRoller.State.POSITION));
 
-    // Driver Right Trigger: Run the Sample Profiled Roller to the requested
-    // position
-    m_driver
-        .rightTrigger()
-        .whileTrue(
-            m_sampleProfiledRollerSubsystem.setStateCommand(SampleProfiledRoller.State.POSITION));
+        // Driver POV Down: Bring Arm and Elevator to Home position
+        m_driver
+            .povDown()
+            .onTrue(
+                Commands.parallel(
+                    m_sampleArmSubsystem.setStateCommand(Arm.State.HOME),
+                    m_sampleElevatorSubsystem.setStateCommand(SampleProfiledElevator.State.HOME)));
 
-    // Driver POV Down: Bring Arm and Elevator to Home position
-    m_driver
-        .povDown()
-        .onTrue(
-            Commands.parallel(
-                m_sampleArmSubsystem.setStateCommand(Arm.State.HOME),
-                m_sampleElevatorSubsystem.setStateCommand(SampleProfiledElevator.State.HOME)));
+        // Driver POV Left: Send Arm and Elevator to LEVEL_1
+        m_driver
+            .povLeft()
+            .onTrue(
+                Commands.parallel(
+                    m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_1),
+                    m_sampleElevatorSubsystem
+                        .setStateCommand(SampleProfiledElevator.State.LEVEL_1)));
 
-    // Driver POV Left: Send Arm and Elevator to LEVEL_1
-    m_driver
-        .povLeft()
-        .onTrue(
-            Commands.parallel(
-                m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_1),
-                m_sampleElevatorSubsystem.setStateCommand(SampleProfiledElevator.State.LEVEL_1)));
+        // Driver POV Up: Send Arm and Elevator to LEVEL_2
+        m_driver
+            .povUp()
+            .onTrue(
+                Commands.parallel(
+                    m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_2),
+                    m_sampleElevatorSubsystem
+                        .setStateCommand(SampleProfiledElevator.State.LEVEL_2)));
 
-    // Driver POV Up: Send Arm and Elevator to LEVEL_2
-    m_driver
-        .povUp()
-        .onTrue(
-            Commands.parallel(
-                m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_2),
-                m_sampleElevatorSubsystem.setStateCommand(SampleProfiledElevator.State.LEVEL_2)));
+        // Driver POV Right: Send Arm and Elevator to LEVEL_3
+        m_driver
+            .povRight()
+            .onTrue(
+                Commands.parallel(
+                    m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_3),
+                    m_sampleElevatorSubsystem
+                        .setStateCommand(SampleProfiledElevator.State.LEVEL_3)));
 
-    // Driver POV Right: Send Arm and Elevator to LEVEL_3
-    m_driver
-        .povRight()
-        .onTrue(
-            Commands.parallel(
-                m_sampleArmSubsystem.setStateCommand(Arm.State.LEVEL_3),
-                m_sampleElevatorSubsystem.setStateCommand(SampleProfiledElevator.State.LEVEL_3)));
+        // Operator X Button: Run the Sample Roller in Eject direction when held
+        m_operator.x()
+            .whileTrue(m_sampleRollersSubsystem.setStateCommand(SampleRollers.State.EJECT));
+        // Operator Y Button: Run the Sample Roller in Intake direction when held
+        m_operator.y()
+            .whileTrue(m_sampleRollersSubsystem.setStateCommand(SampleRollers.State.INTAKE));
+    }
 
-    // Operator Buttons A & B run the Complex and Simple subsystems when held
-    // m_operator.a().whileTrue(m_complexSubsystem.setStateCommand(ComplexSubsystem.State.SCORE));
-    // m_operator.b().whileTrue(m_simpleSubsystem.setStateCommand(SimpleSubsystem.State.ON));
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return m_autoChooser.get();
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand()
+    {
+        return m_autoChooser.get();
+    }
 }
--- src/main/java/frc/robot/subsystems/SampleProfiledRoller/SampleProfiledRollerConstants.java
+++ src/main/java/frc/robot/subsystems/SampleProfiledRoller/SampleProfiledRollerConstants.java
@@ -11,77 +11,78 @@ import frc.robot.subsystems.GenericMotionProfiledSubsystem.GenericMotionProfiled
 /** Add your docs here. */
 public final class SampleProfiledRollerConstants {
 
-  public static final GenericMotionProfiledSubsystemConstants kSubSysConstants =
-      new GenericMotionProfiledSubsystemConstants();
+    public static final GenericMotionProfiledSubsystemConstants kSubSysConstants =
+        new GenericMotionProfiledSubsystemConstants();
 
-  static {
-    kSubSysConstants.kName = "SampleProfiledRoller";
+    static {
+        kSubSysConstants.kName = "SampleProfiledRoller";
 
-    kSubSysConstants.kLeaderMotor = Ports.PROFROLLER_MAIN;
-    kSubSysConstants.kFollowMotor = null;
-    kSubSysConstants.kFollowerOpposesMain = true;
+        kSubSysConstants.kLeaderMotor = Ports.PROFROLLER_MAIN;
+        kSubSysConstants.kFollowMotor = null;
+        kSubSysConstants.kFollowerOpposesMain = true;
 
-    // Using TalonFX internal encoder
+        // Using TalonFX internal encoder
 
-    kSubSysConstants.kCANcoder = null;
-    kSubSysConstants.kMotorConfig.Feedback.FeedbackSensorSource =
-        FeedbackSensorSourceValue.RotorSensor;
-    kSubSysConstants.kMotorConfig.Feedback.SensorToMechanismRatio = 1.0;
-    kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio = 1.0;
+        kSubSysConstants.kCANcoder = null;
+        kSubSysConstants.kMotorConfig.Feedback.FeedbackSensorSource =
+            FeedbackSensorSourceValue.RotorSensor;
+        kSubSysConstants.kMotorConfig.Feedback.SensorToMechanismRatio = 1.0;
+        kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio = 1.0;
 
-    // Using a remote CANcoder
-    /*
-     * kSubSysConstants.kCANcoder = Ports.ARM_CANCODER;
-     * kSubSysConstants.kMotorConfig.Feedback.FeedbackSensorSource =
-     * FeedbackSensorSourceValue.FusedCANcoder;
-     * kSubSysConstants.kMotorConfig.Feedback.SensorToMechanismRatio = 7.04;
-     * kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio = 54.4/7.04;
-     * kSubSysConstants.kEncoderConfig.MagnetSensor.MagnetOffset = 0.3467;
-     * kSubSysConstants.kEncoderConfig.MagnetSensor.SensorDirection =
-     * SensorDirectionValue.Clockwise_Positive;
-     * kSubSysConstants.kEncoderConfig.MagnetSensor.AbsoluteSensorRange =
-     * AbsoluteSensorRangeValue.Unsigned_0To1;
-     */
+        // Using a remote CANcoder
+        /*
+         * kSubSysConstants.kCANcoder = Ports.ARM_CANCODER;
+         * kSubSysConstants.kMotorConfig.Feedback.FeedbackSensorSource =
+         * FeedbackSensorSourceValue.FusedCANcoder;
+         * kSubSysConstants.kMotorConfig.Feedback.SensorToMechanismRatio = 7.04;
+         * kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio = 54.4/7.04;
+         * kSubSysConstants.kEncoderConfig.MagnetSensor.MagnetOffset = 0.3467;
+         * kSubSysConstants.kEncoderConfig.MagnetSensor.SensorDirection =
+         * SensorDirectionValue.Clockwise_Positive;
+         * kSubSysConstants.kEncoderConfig.MagnetSensor.AbsoluteSensorRange =
+         * AbsoluteSensorRangeValue.Unsigned_0To1;
+         */
 
-    kSubSysConstants.kMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    kSubSysConstants.kMotorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-    kSubSysConstants.kMotorConfig.Voltage.PeakForwardVoltage = 12.0;
-    kSubSysConstants.kMotorConfig.Voltage.PeakReverseVoltage = -12.0;
+        kSubSysConstants.kMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        kSubSysConstants.kMotorConfig.MotorOutput.Inverted =
+            InvertedValue.CounterClockwise_Positive;
+        kSubSysConstants.kMotorConfig.Voltage.PeakForwardVoltage = 12.0;
+        kSubSysConstants.kMotorConfig.Voltage.PeakReverseVoltage = -12.0;
 
-    kSubSysConstants.kMotorConfig.CurrentLimits.SupplyCurrentLimit = 40;
-    kSubSysConstants.kMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-    kSubSysConstants.kMotorConfig.CurrentLimits.StatorCurrentLimit = 70;
-    kSubSysConstants.kMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        kSubSysConstants.kMotorConfig.CurrentLimits.SupplyCurrentLimit = 40;
+        kSubSysConstants.kMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        kSubSysConstants.kMotorConfig.CurrentLimits.StatorCurrentLimit = 70;
+        kSubSysConstants.kMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
-    /* REAL system profile constants */
-    kSubSysConstants.kMotorConfig.Slot0.kP = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kI = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kD = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kG = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kS = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kV = 0;
-    kSubSysConstants.kMotorConfig.Slot0.kA = 0;
-    kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 0;
-    kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicAcceleration = 0;
-    kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicJerk = 0;
+        /* REAL system profile constants */
+        kSubSysConstants.kMotorConfig.Slot0.kP = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kI = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kD = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kG = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kS = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kV = 0;
+        kSubSysConstants.kMotorConfig.Slot0.kA = 0;
+        kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 0;
+        kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicAcceleration = 0;
+        kSubSysConstants.kMotorConfig.MotionMagic.MotionMagicJerk = 0;
 
-    /* SIM system profile constants */
-    kSubSysConstants.kSimMotorConfig.Slot0.kP = 700;
-    kSubSysConstants.kSimMotorConfig.Slot0.kI = 0;
-    kSubSysConstants.kSimMotorConfig.Slot0.kD = 100;
-    kSubSysConstants.kSimMotorConfig.Slot0.kG = 13;
-    kSubSysConstants.kSimMotorConfig.Slot0.kS = 0;
-    kSubSysConstants.kSimMotorConfig.Slot0.kV = 0.19;
-    kSubSysConstants.kSimMotorConfig.Slot0.kA = 0;
-    kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 500;
-    kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicAcceleration = 50;
-    kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicJerk = 0;
+        /* SIM system profile constants */
+        kSubSysConstants.kSimMotorConfig.Slot0.kP = 500;
+        kSubSysConstants.kSimMotorConfig.Slot0.kI = 0;
+        kSubSysConstants.kSimMotorConfig.Slot0.kD = 20;
+        kSubSysConstants.kSimMotorConfig.Slot0.kG = 0;
+        kSubSysConstants.kSimMotorConfig.Slot0.kS = 0;
+        kSubSysConstants.kSimMotorConfig.Slot0.kV = 0.19;
+        kSubSysConstants.kSimMotorConfig.Slot0.kA = 0;
+        kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 500;
+        kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicAcceleration = 50;
+        kSubSysConstants.kSimMotorConfig.MotionMagic.MotionMagicJerk = 0;
 
-    // Simulation Type
-    kSubSysConstants.SimType = simType.ROLLER;
+        // Simulation Type
+        kSubSysConstants.SimType = simType.ROLLER;
 
-    // Motor simulation
-    kSubSysConstants.kMotorSimConfig.simMotorModelSupplier = () -> DCMotor.getKrakenX60Foc(1);
-    kSubSysConstants.kMotorSimConfig.simReduction = 1;
-  }
+        // Motor simulation
+        kSubSysConstants.kMotorSimConfig.simMotorModelSupplier = () -> DCMotor.getKrakenX60Foc(1);
+        kSubSysConstants.kMotorSimConfig.simReduction = 1;
+    }
 }
