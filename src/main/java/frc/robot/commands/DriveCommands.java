7b969e504ffbd0c6ce3dd03650326cc7632a35ee
spybh66 <brysonh99@gmail.com>
2025-02-10 13:30:32 -0500

    changing driveatangle to be blue origin

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -149,15 +149,10 @@ public class DriveCommands {
                         linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
                         linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(),
                         omega);
-                boolean isFlipped =
-                    DriverStation.getAlliance().isPresent()
-                        && DriverStation.getAlliance().get() == Alliance.Red;
                 drive.runVelocity(
                     ChassisSpeeds.fromFieldRelativeSpeeds(
                         speeds,
-                        isFlipped
-                            ? drive.getRotation().plus(new Rotation2d(Math.PI))
-                            : drive.getRotation()));
+                        drive.getRotation()));
             },
             drive)
 
