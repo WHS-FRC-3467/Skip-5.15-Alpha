f7e10daa2f6a150fbbd5defad26d8bb04e1cc58d
Driver3467 <frc_3467_driver@team3467.org>
2025-02-15 15:17:09 -0500

    camera-offsets

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -31,10 +31,10 @@ public class VisionConstants {
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
     public static Transform3d robotToCamera0 =
-        new Transform3d(0.289591, 0.261649, 0.205829,
+        new Transform3d(0.293688, 0.346515, 0.219053,
             new Rotation3d(0.0, Units.degreesToRadians(-15), 0.0));
     public static Transform3d robotToCamera1 =
-        new Transform3d(0.289591, -0.261649, 0.205829,
+        new Transform3d(0.293688, -0.346515, 0.219053,
             new Rotation3d(0.0, Units.degreesToRadians(-15), 0.0));
 
     // Basic filtering thresholds
