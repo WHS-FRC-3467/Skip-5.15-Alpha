b11e6009d7e62da331140ff87c127aae296f26b7
Driver3467 <frc_3467_driver@team3467.org>
2025-02-15 11:06:24 -0500

    merge arm

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -17,35 +17,15 @@ import lombok.Setter;
 @Getter
 public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
 
-<<<<<<< HEAD
+    static LoggedTunableNumber positionTuning =
+        new LoggedTunableNumber("Arm/PositionTuningSP", 124.0);
+
     // .14 rot is the max extension
-=======
-    static LoggedTunableNumber positionTuning = new LoggedTunableNumber("Arm/PositionTuningSP", 124.0);
->>>>>>> main
 
     @RequiredArgsConstructor
     @Getter
     public enum State implements TargetState {
         // HOMING(0.0, 0.0, ProfileType.MM_POSITION),
-<<<<<<< HEAD
-        STOW(Units.degreesToRotations(124.0), 0.0, ProfileType.MM_POSITION),
-
-        CORAL_INTAKE(Units.degreesToRotations(144.0), 0.0, ProfileType.MM_POSITION),
-        LEVEL_1(Units.degreesToRotations(120.0), 0.0, ProfileType.MM_POSITION),
-        LEVEL_2(Units.degreesToRotations(105.0), 0.0, ProfileType.MM_POSITION),
-        LEVEL_3(Units.degreesToRotations(105.0), 0.0, ProfileType.MM_POSITION),
-        LEVEL_4(Units.degreesToRotations(90.0), 0.0, ProfileType.MM_POSITION),
-        CLIMB(.14, 0.0, ProfileType.MM_POSITION),
-        ALGAE_LOW(Units.degreesToRotations(114.0), 0.0, ProfileType.MM_POSITION),
-        ALGAE_HIGH(Units.degreesToRotations(114.0), 0.0, ProfileType.MM_POSITION),
-        ALGAE_GROUND(Units.degreesToRotations(9.0), 0.0, ProfileType.MM_POSITION),
-        ALGAE_SCORE(Units.degreesToRotations(114.0), 0.0, ProfileType.MM_POSITION),
-        BARGE(Units.degreesToRotations(30.0), 0.0, ProfileType.MM_POSITION),
-        CHARACTERIZATION(0.0, 0.0, ProfileType.CHARACTERIZATION);
-
-        private final double output;
-        private final double feedFwd;
-=======
         STOW(() -> Units.degreesToRotations(124.0), ProfileType.MM_POSITION),
         CORAL_INTAKE(() -> Units.degreesToRotations(144.0), ProfileType.MM_POSITION),
         LEVEL_1(() -> Units.degreesToRotations(120.0), ProfileType.MM_POSITION),
@@ -58,9 +38,9 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
         ALGAE_GROUND(() -> Units.degreesToRotations(9.0), ProfileType.MM_POSITION),
         ALGAE_SCORE(() -> Units.degreesToRotations(114.0), ProfileType.MM_POSITION),
         BARGE(() -> Units.degreesToRotations(30.0), ProfileType.MM_POSITION),
-        TUNING(() -> Units.degreesToRotations(positionTuning.getAsDouble()), ProfileType.MM_POSITION),
+        TUNING(() -> Units.degreesToRotations(positionTuning.getAsDouble()),
+            ProfileType.MM_POSITION),
         CHARACTERIZATION(() -> 0.0, ProfileType.CHARACTERIZATION);
->>>>>>> main
 
         private final DoubleSupplier output;
         private final ProfileType profileType;
