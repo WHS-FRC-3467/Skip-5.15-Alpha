869c74f2942bb8cb56f21c9db771b86bee460388
skukshtel <scottkukshtel@team3467.org>
2025-03-18 17:44:58 -0400

    Fixed merge errors + LED animation fixes

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -19,17 +19,17 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
     public enum Setpoints {
         STOW(Units.degreesToRotations(125.18), Units.degreesToRotations(125.18)),
         CORAL_INTAKE(Units.degreesToRotations(137.7), Units.degreesToRotations(137.7)),
-        CORAL_CLEARAMCE(Units.degreesToRotations(133.5), Units.degreesToRotations(133.5)),
+        // CORAL_CLEARANCE(Units.degreesToRotations(133.5), Units.degreesToRotations(133.5)),
         LEVEL_1(Units.degreesToRotations(94.13), Units.degreesToRotations(94.13)),
         LEVEL_2(Units.degreesToRotations(94.48), Units.degreesToRotations(94.48)),
-        LEVEL_3(Units.degreesToRotations(94.48), Units.degreesToRotations(94.48)),
+        LEVEL_3(Units.degreesToRotations(104.48), Units.degreesToRotations(104.48)),
         LEVEL_4(Units.degreesToRotations(101.33), Units.degreesToRotations(101.33)),
-        CLIMB(Units.degreesToRotations(50.4), Units.degreesToRotations(50.4)),
-        ALGAE_LOW(.2377, .2377),
-        ALGAE_HIGH(.2446, .2446),
+        CLIMB(Units.degreesToRotations(82.4), Units.degreesToRotations(82.4)),
+        ALGAE_LOW(Units.degreesToRotations(103.3), Units.degreesToRotations(103.3)),
+        ALGAE_HIGH(Units.degreesToRotations(103.3), Units.degreesToRotations(103.3)),
         ALGAE_GROUND(Units.degreesToRotations(70.0), Units.degreesToRotations(70.0)),
-        ALGAE_SCORE(Units.degreesToRotations(120.0), Units.degreesToRotations(120.0)),
-        BARGE(Units.degreesToRotations(140.0), Units.degreesToRotations(140.0));
+        PROCESSOR_SCORE(Units.degreesToRotations(120.0), Units.degreesToRotations(120.0)),
+        BARGE(Units.degreesToRotations(130.0), Units.degreesToRotations(130.0));
 
         private final double gortSetpoint;
         private final double bajaSetpoint;
@@ -48,19 +48,20 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
     @Getter
     public enum State implements TargetState {
         // HOMING(0.0, 0.0, ProfileType.MM_POSITION),
-        STOW(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(125.18), 0)),
+        STOW(new ProfileType.MM_POSITION(() -> Setpoints.STOW.getSetpoint(), 0)),
         // CORAL_INTAKE(() -> 0.42, ProfileType.MM_POSITION),
-        CORAL_INTAKE(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(137.7), 0)),
-        LEVEL_1(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(94.13), 0)),
-        LEVEL_2(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(94.48), 0)),
-        LEVEL_3(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(104.48), 0)),
-        LEVEL_4(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(101.33), 0)),
-        CLIMB(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(82.4), 0)),
-        ALGAE_LOW(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(103.3), 0)),
-        ALGAE_HIGH(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(103.3), 0)),
-        ALGAE_GROUND(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(70.0), 0)),
-        ALGAE_SCORE(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(120.0), 0)),
-        BARGE(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(130.0), 0)),
+        CORAL_INTAKE(new ProfileType.MM_POSITION(() -> Setpoints.CORAL_INTAKE.getSetpoint(), 0)),
+        LEVEL_1(new ProfileType.MM_POSITION(() -> Setpoints.LEVEL_1.getSetpoint(), 0)),
+        LEVEL_2(new ProfileType.MM_POSITION(() -> Setpoints.LEVEL_2.getSetpoint(), 0)),
+        LEVEL_3(new ProfileType.MM_POSITION(() -> Setpoints.LEVEL_3.getSetpoint(), 0)),
+        LEVEL_4(new ProfileType.MM_POSITION(() -> Setpoints.LEVEL_4.getSetpoint(), 0)),
+        CLIMB(new ProfileType.MM_POSITION(() -> Setpoints.CLIMB.getSetpoint(), 0)),
+        ALGAE_LOW(new ProfileType.MM_POSITION(() -> Setpoints.ALGAE_LOW.getSetpoint(), 0)),
+        ALGAE_HIGH(new ProfileType.MM_POSITION(() -> Setpoints.ALGAE_HIGH.getSetpoint(), 0)),
+        ALGAE_GROUND(new ProfileType.MM_POSITION(() -> Setpoints.ALGAE_GROUND.getSetpoint(), 0)),
+        PROCESSOR_SCORE(
+            new ProfileType.MM_POSITION(() -> Setpoints.PROCESSOR_SCORE.getSetpoint(), 0)),
+        BARGE(new ProfileType.MM_POSITION(() -> Setpoints.BARGE.getSetpoint(), 0)),
         CHARACTERIZATION(new ProfileType.CHARACTERIZATION()),
         COAST(new ProfileType.DISABLED_COAST()),
         BRAKE(new ProfileType.DISABLED_BRAKE());
