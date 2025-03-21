067d58534c7c20a9035792fc1787532fb22b4c3a
skukshtel <scottkukshtel@team3467.org>
2025-03-20 22:11:16 -0400

    Fix merge error

--- src/main/java/frc/robot/subsystems/Claw/ClawRoller/ClawRoller.java
+++ src/main/java/frc/robot/subsystems/Claw/ClawRoller/ClawRoller.java
@@ -17,11 +17,10 @@ public class ClawRoller
     public final Trigger stalled =
         new Trigger(
             () -> (Math.abs(super.inputs.velocityRps) <= 0.02
-                && Math.abs(super.inputs.supplyCurrentAmps[0]) >= 1));
+                && super.inputs.supplyCurrentAmps[0] >= 1));
 
     public final Trigger stopped =
         new Trigger(() -> (Math.abs(super.inputs.velocityRps) <= 0.02));
-                && Math.abs(super.inputs.supplyCurrentAmps[0]) >= 1));
 
     @RequiredArgsConstructor
     @Getter
